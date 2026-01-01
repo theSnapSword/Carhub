b09bc2848e022c7122f72610a03e32198ddf8bd9
Hamza <hamzaabouljid@gmail.com>
2026-01-02 00:21:48 +0100

    fix a litle probleme with the mobile mode

---
+++ style/header.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (hamburger && navLinks) {
+        hamburger.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+            hamburger.classList.toggle('toggle');
+        });
+    }
+});
